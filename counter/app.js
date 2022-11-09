170a76d153112783e656e397e46fd9f78231ea74
lokesh jadhav <106864522+lokesh7887@users.noreply.github.com>
2022-11-10 00:55:03 +0530

    Add files via upload

---
+++ counter/app.js
@@ -0,0 +1,24 @@
+//set initial count
+let count = 0;
+
+//select vlaue and buttons
+const value = document.querySelector('#value');
+
+const btn= document.querySelectorAll(".btn");
+
+btn.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        const styles =e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increase")){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        value.textContent= count;
+    });
+});
+
